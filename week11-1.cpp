f962abaabf3d146be41b4cf11f4109be5ccbfbd3
13750023 <13750023@me.mcu.edu.tw>
2025-04-30 11:59:05 +0800

    week11

---
+++ week11-1.cpp
@@ -0,0 +1,17 @@
+///week11-1.cpp
+///рSOIT107_ADANCE_009徊芅琜暗ㄓ
+#include <stdio.h>
+int main()
+{
+    printf("叫块1422: ");
+    int n;
+    scanf("%d",&n);
+    ///Q:或т程计?
+    int ans = 0;
+    while(n>0){
+        printf("瞷ㄓブ:%d\n", n%10);
+        if(n%10 > ans) ans = n%10;///ブ猭–计
+        n = n / 10;
+    }
+    printf("т程ブ,琌: %d\n", ans);
+}
